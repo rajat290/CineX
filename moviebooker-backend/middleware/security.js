8ce23ca2f3c05b4c1fdeffa939a42f50a871f273
rajat <rajat290@users.noreply.github.com>
2025-08-31 12:20:43 +0530

    updated the authLimiter timming

--- moviebooker-backend/middleware/security.js
+++ moviebooker-backend/middleware/security.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 
 // Rate limiting
 const authLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 15 minutes
-  max: 50, // 5 attempts
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // 5 attempts
   message: 'Too many attempts, please try again later',
   skipSuccessfulRequests: true
 });
